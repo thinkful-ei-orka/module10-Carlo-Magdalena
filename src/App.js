7a25139e43c69ecac11ddb9a8e2235807efbb5e6
Carlo Paredes <c.nick.paredes@gmail.com>
2020-05-13 14:00:28 -0700

    Added small change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HelloWorld from './state-drills/HelloWorld'; 
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
+import Accordion from './state-drills/Accordion';
 
 // function App() {
 //   return (
